4f6421d4e6076352fe6e1072aeee761649e78c73
Marco Tammaro <marcotammaro42@gmail.com>
2021-10-22 19:17:08 +0200

    Introducing possible light theme palette

--- lib/palette.dart
+++ lib/palette.dart
@@ -6,3 +6,11 @@ class Palette {
   static const secondaryColor = Color(0xFFFFFFFF);
   static const tertiaryColor = Color(0xFF8E9095);
 }
+
+/// Possible light theme palette
+// class Palette {
+//   static const mainColor = Color(0xFF139414);
+//   static const secondaryColor = Color(0xFF20222A);
+//   static const backgroundColor = Color(0xFFFFFFFF);
+//   static const tertiaryColor = Color(0xFF8E9095);
+// }
